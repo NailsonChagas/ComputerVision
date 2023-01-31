062bb95eddbf91611ea4100942c38daa56e8ffc3
Nailson Chagas <chagasnailson@gmail.com>
2023-01-31 16:02:36 -0300

    feat: iniciando processamento de imagens

---
+++ 3_img_processing/main.py
@@ -0,0 +1,18 @@
+print("""
+--------- Processamento de Imagens ---------\n
+Neste módulo será aprendido:
+- Operações de processamento de imagens
+- Execução de operações como:
+    * Smoothing
+    * Blurring
+    * Operações Morfológicas
+- Obter propriedades como histogramas e color space
+""")
+
+print("""
+--------- Mapeamentos de cor ---------\n
+- RGB: Red Green Blue
+- RGBA: Red Green Blue Alpha
+- HSV: Hue Saturation Value
+- HSL: Hue Saturation Lightness
+""")
