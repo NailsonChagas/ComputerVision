e81884a0471a53352ddcd57836bb62083a3c6332
Nailson Chagas <chagasnailson@gmail.com>
2023-01-31 15:49:46 -0300

    feat: desenhando continuamente

--- 2_img_basics_with_openCV/drawing.py
+++ 2_img_basics_with_openCV/drawing.py
@@ -33,12 +33,24 @@ cv2.destroyAllWindows()
 print("\n--------- Desenhando com o mouse 2 ---------\n")
 
 blank_img = np.zeros(shape=(512,512,3), dtype=np.int8)
+drawing = False
+ix, iy = -1, -1
 
-def draw(event, x, y, flags, param):
-    pass
+def draw_rect(event, x, y, flags, param):
+    global ix, iy, drawing
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            cv2.rectangle(blank_img, (ix,iy), (x,y), (255,0,0), 1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(blank_img, (ix,iy), (x,y), (255,0,0), -1)
+
 
 cv2.namedWindow(winname="DESENHAR")
-cv2.setMouseCallback("DESENHAR", draw)
+cv2.setMouseCallback("DESENHAR", draw_rect)
 
 while True:
     cv2.imshow("DESENHAR", blank_img)
