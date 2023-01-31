8db83bee927b38c8d9f2721bdab1cee1e3da7b1b
Nailson Chagas <chagasnailson@gmail.com>
2023-01-31 14:11:51 -0300

    feat: lendo imagens com o opencv

--- 2_img_basics_with_openCV/main.py
+++ 2_img_basics_with_openCV/main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import cv2
 
 """
@@ -13,3 +15,26 @@ Programada em: C++
 É uma biblioteca focada principalmente em visão computacional em tempo real
 """)
 
+print("\n--------- Lendo imagens com OpenCV ---------\n")
+img_BGR = cv2.imread('./puppy.jpg') 
+print(type(img_BGR), img_BGR.shape)
+
+plt.imshow(img_BGR)
+plt.show()
+
+"""
+A imagem é diferente da original devido a difereças entre 
+como o matplotlib e opencv lidam com a ordem dos canais de cores 
+Matplot -> RBG
+OpenCV -> BGR
+"""
+
+img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+print(type(img_RGB), img_RGB.shape)
+plt.imshow(img_RGB)
+plt.show()
+
+img_gray = cv2.imread('./puppy.jpg', cv2.IMREAD_GRAYSCALE)
+print(type(img_gray), img_gray.shape)
+plt.imshow(img_gray, cmap='gray')
+plt.show()
