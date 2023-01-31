80acb2967229cbc84c21ac27a3a68eb40bb71200
Nailson Chagas <chagasnailson@gmail.com>
2023-01-31 13:57:33 -0300

    feat: o que é openCV?

--- 2_img_basics_with_openCV/main.py
+++ 2_img_basics_with_openCV/main.py
@@ -3,3 +3,11 @@ Objetivos da seção:
     - Começar a entender o uso do OpenCV
     - Abrir imagens e desenhar em cima delas
 """
+
+print("""
+\n--------- OpenCV ---------\n
+OpenCV: Open Source Computer Vision
+Programada em: C++
+É uma biblioteca focada principalmente em visão computacional em tempo real
+""")
+
