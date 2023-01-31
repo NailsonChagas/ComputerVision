5fcf9fd20c3e3b59907ad9ba88bf6a5ac5a7223f
Nailson Chagas <chagasnailson@gmail.com>
2023-01-31 16:58:38 -0300

    feat: blended

--- 3_img_processing/main.py
+++ 3_img_processing/main.py
@@ -23,9 +23,24 @@ print("""
 print("--------- Convertendo Mapeamentos de cor ---------\n")
 img_BGR = cv2.imread("./dog.jpg")
 img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+print(img_RGB.shape)
 plt.imshow(img_RGB)
 plt.show()
 
 img_HSV = cv2.cvtColor(img_RGB, cv2.COLOR_RGB2HSV)
+print(img_HSV.shape)
 plt.imshow(img_HSV)
+plt.show()
+
+print("\n--------- Blending and Pasting: Combinando Imagens ---------")
+print("""
+- Blending: feito através da função addWeighted, que pega as 
+imagens e as combina. Blend é feito seguindo a formula: 
+    * new_pixel = (alpha x pixel_1) + (beta x pixel_2) + gamma
+""")
+DOG1 = img_RGB.copy()
+DOG2 = cv2.flip(DOG1, 1)
+
+blended = cv2.addWeighted(DOG1, 0.5, DOG2, 0.5, 0)
+plt.imshow(blended)
 plt.show()
