e6ef332c42c50c05f1f6066230280048b7de8d3f
Nailson Chagas <chagasnailson@gmail.com>
2023-02-01 17:13:34 -0300

    feat: resumo de detecção de objetos

---
+++ 5_object_detection/main.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+print("""
+----------- Detecção de Objetos -----------\n
+Objetivos da seção:
+- Entender diversos modos de de detecção de objetos 
+
+Tipos de de detecção de objetos:
+1 - Template Matching: 
+    * Procurar uma cópia exata de uma imagem em outra imagem
+2 - Corner Detection:
+    * Procurar por cantos na imagem
+3 - Edge Detection:
+    * Procurar contornos generalizados de objetos
+4 - Grid Detection:
+    * Combina os metodos 2 e 3 para encontrar grids na imagem   
+    * Utilizado em varias aplicações
+5 - Contour Detection:
+    * Permite detectar o primeiro plano VS plano de fundo
+    * Permite detectar contornos externos VS internos
+6 - Feature matching:
+    * Mais avançado meio de detectar objetos em imagens
+    * Permite detectar um objeto, mesmo que na imagem não se possua
+    uma cópia exata do objeto sendo procurado
+7 - Watershed Algorithm:
+    * Algoritmo avançado que permite segmentar uma imagem entre
+    o primeiro plano e o segundo plano
+8 - Facial and Eye Detection:
+    * Utiliza Haar Cascades para detectar faces em imagens
+    * Isso não é reconhecimento facial (para isso será utilizado Deep Learn)
+""")
+
