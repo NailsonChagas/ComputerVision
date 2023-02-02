2f0747f4ea14dc86b0a07910883449908e53fe04
Nailson Chagas <chagasnailson@gmail.com>
2023-02-02 15:53:50 -0300

    feat: Shi-Tomasi Corner Detection

--- 5_object_detection/main.py
+++ 5_object_detection/main.py
@@ -116,7 +116,7 @@ if CornerDetection:
         * Shi-Tomasi Corner Detection:
             $ É uma modificação do Harris Corner Detection
     """)
-
+    print("---------- Harris Corner Detection --------------")
     flat_chess = cv2.imread("./flat_chessboard.png")
     flat_chess = cv2.cvtColor(flat_chess, cv2.COLOR_BGR2RGB)
     plt.imshow(flat_chess)
@@ -146,5 +146,34 @@ if CornerDetection:
     dst = cv2.cornerHarris(gray, blockSize=2, ksize=3, k=0.04)
     dst = cv2.dilate(dst, None)
     real_chess[dst>0.01*dst.max()] = [255,0,0]
+    plt.imshow(real_chess)
+    plt.show() 
+
+    print("---------- Shi-Tomasi Corner Detection --------------")
+    flat_chess = cv2.imread("./flat_chessboard.png")
+    flat_chess = cv2.cvtColor(flat_chess, cv2.COLOR_BGR2RGB)
+    gray_flat_chess = cv2.cvtColor(flat_chess, cv2.COLOR_RGB2GRAY)
+
+    real_chess = cv2.imread("./real_chessboard.jpg")
+    real_chess = cv2.cvtColor(real_chess, cv2.COLOR_BGR2RGB)
+    gray_real_chess = cv2.cvtColor(real_chess, cv2.COLOR_RGB2GRAY)
+
+    corners = cv2.goodFeaturesToTrack(gray_flat_chess, 64, 0.01, 10)
+    corners = np.intp(corners)
+
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(flat_chess, (x,y), 3, (255,0,0), -1)
+    
+    plt.imshow(flat_chess)
+    plt.show() 
+
+    corners = cv2.goodFeaturesToTrack(gray_real_chess, 100, 0.01, 10)
+    corners = np.intp(corners)
+
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(real_chess, (x,y), 3, (255,0,0), -1)
+    
     plt.imshow(real_chess)
     plt.show() 
