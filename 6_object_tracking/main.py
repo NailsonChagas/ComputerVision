40f07ff4f3974698e21ac510a3e09b61263c857c
Nailson Chagas <chagasnailson@gmail.com>
2023-02-05 15:37:24 -0300

    feat: optical flow teoria

--- 6_object_tracking/main.py
+++ 6_object_tracking/main.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+
+print("""
+----------- Object Tracking -----------\n
+- Tecnicas:
+    * Optical Flow:
+        + É o padrão de movimento aparente de objetos na imagem
+        entre dois frames consecutivos, causado por movimento do
+        objeto ou da camera
+    * MeanShift e CamShift
+- Entender tecnicas mais avançadas:
+    * Testar APIS incluidas 
+""")
